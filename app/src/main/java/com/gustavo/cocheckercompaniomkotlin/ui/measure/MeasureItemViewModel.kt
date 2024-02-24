455594ff4f35774e8ce3fd5f8be9762b9c51be7a
Gustavo Amaral Eliseu <30469845+GustavoEliseu@users.noreply.github.com>
2024-02-24 05:05:00 -0300

    adds timeStamp to measure layout

--- app/src/main/java/com/gustavo/cocheckercompaniomkotlin/ui/measure/MeasureItemViewModel.kt
+++ app/src/main/java/com/gustavo/cocheckercompaniomkotlin/ui/measure/MeasureItemViewModel.kt
@@ -5,7 +5,9 @@ import com.gustavo.cocheckercompaniomkotlin.model.data.MeasureItem
 
 class MeasureItemViewModel {
     val mutableMeasureUID : MutableLiveData<String?> = MutableLiveData(null)
+    val mutableMeasureTimeStamp : MutableLiveData<String?> = MutableLiveData(null)
     fun bind(measure: MeasureItem){
         mutableMeasureUID.value = measure.CO2.toString()
+        mutableMeasureTimeStamp.value = measure.TimeStamp
     }
 }
